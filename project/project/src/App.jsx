c4dd21d238ba9875211f316ac317615104ffafec
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-11-18 15:32:43 +0100

    feat: Added addGuessedLetter function

--- project/project/src/App.jsx
+++ project/project/src/App.jsx
@@ -8,6 +8,12 @@ import "./App.css";
 export default function App() {
 
     const [currentWord, setCurrentWord] = React.useState("react")
+    const [guessedLetters, setGuessedLetters] = React.useState([])
+    console.log(guessedLetters)
+
+    const addGuessedLetter = (letter) => {
+        setGuessedLetters(prevLetters => [...prevLetters, letter])
+    }
 
     const wordArr = currentWord.toUpperCase().split('').map((letter, index) => {
         return (
@@ -26,8 +32,15 @@ export default function App() {
             <section className="letter-box">
                 {wordArr}
             </section>
-            <Keyboard />
+            <Keyboard
+                onClick={() => addGuessedLetter(letter)}
+            />
             <button className="new-game">New Game</button>
         </main>
     )
 }
+
+
+//You're very close!
+// You've set up the state and the function correctly, but there's a small issue with how you're passing the letter to the `addLetter` function.
+// When you click a button, you need to pass the specific letter that was clicked—right now, the function isn't receiving that information. How can you modify the `onClick` handler to pass the letter?
